f47d3f998402f5cf8a55661abd32a56f193575fa
rajkumarvoleti <voletirajkumar@gmail.com>
2023-06-17 17:25:43 +0530

    gravity project done

--- canvas.js
+++ canvas.js
@@ -6,74 +6,14 @@ var c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// c.fillStyle = 'red';
-// c.fillRect(100,100,100,100);
-
-// c.beginPath();
-// c.moveTo(300,300);
-// c.lineTo(300,600);
-// c.lineTo(600,300);
-// c.strokeStyle = "blue";
-// c.stroke();
-
-
-// for (let i = 0; i < 10; i++) {
-//   var x = Math.random()*innerWidth;
-//   var y = Math.random()*innerHeight;
-//   c.beginPath();
-//   c.strokeStyle = "green";
-//   c.arc(x,y,100,0,2*pi,false);
-//   c.stroke();
-// }
-
-// var x = Math.random()*innerWidth;
-// var y = Math.random()*innerHeight;
-// var dx= (Math.random()-0.5)*10;
-// var dy = (Math.random()-0.5)*10;
-// var r = 30;
-
-// function animate() {
-//   requestAnimationFrame(animate);
-
-//   // draw circle
-//   c.clearRect(0,0,innerWidth,innerHeight);
-//   c.beginPath();
-//   c.arc(x,y,r,0,2*pi,false);
-//   c.strokeStyle = "green";
-//   c.stroke();
-
-//   // changing direction
-//   if(x + r > innerWidth || x - r < 0)
-//     dx = -dx;
-//   if(y + r > innerHeight || y - r < 0)
-//     dy = -dy;
-
-//   // updating x,y values
-//   x += dx;
-//   y += dy
-// }
-
-// animate();
-
-var mouse = {
-  x:undefined,
-  y:undefined
-};
-
 window.addEventListener('resize',function() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   init();
 })
 
-window.addEventListener('mousemove',function(e){
-  mouse.x = e.x;
-  mouse.y = e.y;
-})
-
-window.addEventListener('mouseout',function(e){
-  mouse.x = undefined;
-  mouse.y = undefined;
+window.addEventListener("click",function() {
+  init();
 })
 
 const colors = ["#73EEDC","#D9BBF9","#E6AF2E","#23395B","#CE4760"];
@@ -83,11 +23,18 @@ function generateRandomColor() {
   return colors[index];
 }
 
-function Circle(x, y, r, dx, dy){
+function generateRandomInt(x, y){
+  return Math.floor(Math.random()*(y-x+1)) + x;
+}
+
+const gravity = 0.9;
+const frictionY = 0.8;
+const frictionX = 0.8;
+
+function Ball(x, y, r, dx, dy){
   this.x = x;
   this.y = y;
   this.r = r;
-  this.minRadius = r;
   this.dx = dx;
   this.dy = dy;
   this.color = generateRandomColor();
@@ -96,40 +43,37 @@ function Circle(x, y, r, dx, dy){
     c.beginPath();
     c.arc(this.x,this.y,this.r,0,2*pi,false);
     c.fillStyle = this.color;
+    c.stroke();
     c.fill();
   }
 
   this.update = function(){
 
     // update position
-    if(this.x + this.r > innerWidth || this.x - this.r < 0)
-      this.dx = -this.dx;
-    if(this.y + this.r > innerHeight || this.y - this.r < 0)
-      this.dy = -this.dy;
+    if(this.x + this.dx + this.r > innerWidth || this.x - this.r < 0)
+      this.dx = -this.dx*frictionX;
+    if(this.y + this.dy + this.r > innerHeight)
+      this.dy = -this.dy*frictionY;
+    else
+      this.dy += gravity;
+    console.log(dy);
     this.x += this.dx;
     this.y += this.dy;
-
-    // update radius (interaction)
-    if(Math.abs(mouse.x-this.x) < 50 && Math.abs(mouse.y-this.y) < 50)
-      this.r = Math.min(maxRadius,this.r+1);
-    else
-      this.r = Math.max(this.minRadius,this.r-1);
 
   }
 }
 
-var circles = [];
-var maxRadius = 50;
+var balls = [];
 function init(){
-  circles = [];
-  var numberOfCircles = (innerWidth*innerHeight)/500;
-  for (let i = 0; i < numberOfCircles; i++) {
-    var x = Math.random()*(innerWidth - 2*r) + r;
-    var y = Math.random()*(innerHeight - 2*r) + r;
-    var r = Math.random()*4 + 1;
-    var dx= (Math.random()-0.5)*4;
-    var dy = (Math.random()-0.5)*4;
-    circles.push(new Circle(x,y,r,dx,dy));
+  balls = [];
+  var numberOfBalls = (innerWidth*innerHeight)/2000;
+  for (let i = 0; i < numberOfBalls; i++) {
+    var r = generateRandomInt(5,20);
+    var x = generateRandomInt(r,innerWidth-r);
+    var y = generateRandomInt(r,innerHeight-r);
+    var dx= generateRandomInt(-3,3);
+    var dy = generateRandomInt(-3,3);
+    balls.push(new Ball(x,y,r,dx,dy));
   }
 }
 
@@ -137,9 +81,9 @@ function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0,0,innerWidth,innerHeight);
 
-  circles.forEach(circle => {
-    circle.draw();
-    circle.update();
+  balls.forEach(ball => {
+    ball.draw();
+    ball.update();
   });
 
 }
